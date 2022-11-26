f4f1071e6efd7b078f68b0bd398c08f55e332497
YukiShinji <lex.lipov@mail.ru>
2022-11-26 17:27:16 +0300

    3 task is done

--- seminar_2.py
+++ seminar_2.py
@@ -24,3 +24,16 @@ def productsOfNumbers (Number):
 userNumber = int(input("Enter number: "))
 productsOfNumbers(userNumber)
 """
+
+# 3 Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму
+"""
+def listConsequence(n):
+    summ = [((1 + 1/i)**i) for i in range(1, n + 1)]
+    return sum(summ)
+
+userNumber = int(input("Enter number: "))
+print(listConsequence(userNumber))
+"""
+
+# 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных индексах. Индексы вводятся одной строкой, через пробел
+
