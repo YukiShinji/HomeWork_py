aac1d086b9f6556d3e9f65bd0d44cb4d6deafbc9
YukiShinji <lex.lipov@mail.ru>
2022-11-26 15:54:20 +0300

    2 task is done

--- seminar_1.py
+++ seminar_1.py
@@ -1,5 +1,5 @@
-# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
-
+# 1 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+"""
 def dayOfTheWeek (userNum):
     if (userNum >= 1 and userNum <= 5):
         print ("This day not a weekend")
@@ -11,3 +11,11 @@ def dayOfTheWeek (userNum):
 print ("Enter the day of the week")
 userNumber = int(input ())
 dayOfTheWeek (userNumber)
+"""
+# 2 Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+"""
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(x, y, z, (not (x or y or z) == (not x and not y and not z)))
+"""
