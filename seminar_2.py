f0b45901f5742ebae2452a266e327e5ac4e9f44e
YukiShinji <lex.lipov@mail.ru>
2022-11-26 17:17:32 +0300

    2 task is done

--- seminar_2.py
+++ seminar_2.py
@@ -11,3 +11,16 @@ def  Sumnumber(Number):
 userNumber = int(input("Enter number: "))
 print(Sumnumber(userNumber))
 """
+
+# 2 Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+"""
+def productsOfNumbers (Number):
+    cout = 1
+    for i in range(Number):
+        i = i + 1
+        cout = i * cout
+        print (cout, end=" ")
+
+userNumber = int(input("Enter number: "))
+productsOfNumbers(userNumber)
+"""
