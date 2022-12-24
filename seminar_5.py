abe9158fa5f26dec53f90f9ca8c1934301c0eb00
YukiShinji <lex.lipov@mail.ru>
2022-12-24 17:38:58 +0300

    HW 5 is done

---
+++ seminar_5.py
@@ -0,0 +1,241 @@
+# 1 Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+'''
+text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+    этого абв текста все слова, содерабващие содержащие "абв"'
+
+def del_some_words(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+text = del_some_words(text)
+print(text)
+'''
+# 2 Создайте программу для игры с конфетами человек против человека.
+'''
+from random import *
+import os
+
+
+welcome_text = ('Приветствую Вас, маленькие любители сладкого!\n'
+                'Хотите сыграть в игру "2021 шаг в сторону сахарного диабета"?\n'
+                'Для начала я расскажу правила:\n'
+                'Я даю Вам 2021 конфету, вы берете их поочереди,\n'
+                'причем, за один раз можно взять не больше 28 конфет.\n'
+                'Выигрывает тот, кто последним ходом заберет все конфеты.\n'
+                'Ну что начнем?')
+print(welcome_text)
+
+message = ['твоя очередь', 'да бери уже', 'бери больше', 'не корову проигрываешь',
+           'бери быстрее', 'да харош, так долго думать уже']
+
+
+def player_vs_player():
+    candies_total = 2021
+    max_take = 28
+    count = 0
+    player_1 = input('\nКак тебя зовут?: ')
+    player_2 = input('\nА твоего соперника?: ')
+
+    print(f'\nНу чтож {player_1} и  {player_2} да начнется игра !\n')
+    print('\nДля начала опеределим кто первый начнет игру.\n')
+
+    x = randint(1, 2)
+    if x == 1:
+        lucky = player_1
+        loser = player_2
+    else:
+        lucky = player_2
+        loser = player_1
+    print(f'Поздравляю {lucky} ты ходишь первым !')
+
+    while candies_total > 0:
+        if count == 0:
+            step = int(input(f'\n{choice(message)} {lucky} = '))
+            if step > candies_total or step > max_take:
+                step = int(input(
+                    f'\nНе жадничай можно взять только {max_take} конфет {lucky}, попробуй еще раз: '))
+            candies_total = candies_total - step
+        if candies_total > 0:
+            print(f'\nна кону еще {candies_total}')
+            count = 1
+        else:
+            print('Все кончились конфетки')
+
+        if count == 1:
+            step = int(input(f'\n{choice(message)}, {loser} '))
+            if step > candies_total or step > max_take:
+                step = int(input(
+                    f'\nНе жадничай можно взять только {max_take} конфет {loser}, попробуй еще раз: '))
+            candies_total = candies_total - step
+        if candies_total > 0:
+            print(f'\nна кону еще {candies_total}')
+            count = 0
+        else:
+            print('Баста, карапузик, кончились конфетки')
+
+    if count == 1:
+        print(f'{loser} ПОБЕДИЛ')
+    if count == 0:
+        print(f'{lucky} ПОБЕДИЛ')
+
+
+player_vs_player()
+
+
+def player_vs_bot():
+    candies_total = 2021
+    max_take = 28
+    player_1 = input('\nКак тебя зовут?: ')
+    player_2 = 'Компьютер'
+    players = [player_1, player_2]
+    print(f'\nНу чтож {player_1} и  {player_2} да начнется игра !\n')
+    print('\nДля начала опеределим кто первый начнет игру.\n')
+
+    lucky = randint(-1, 0)
+
+    print(f'Поздравляю {players[lucky+1]} ты ходишь первым !')
+
+    while candies_total > 0:
+        lucky += 1
+
+        if players[lucky % 2] == 'Компьютер':
+            print(
+                f'\nХодит {players[lucky%2]} \nНа кону {candies_total}. \n{choice(message)}: ')
+
+            if candies_total < 29:
+                step = candies_total
+            else:
+                delenie = candies_total//28
+                step = candies_total - ((delenie*max_take)+1)
+                if step == -1:
+                    step = max_take -1
+                if step == 0:
+                    step = max_take
+            while step > 28 or step < 1:
+                step = randint(1,28)
+            print(step)
+        else:
+            step = int(input(f'\nНу чтож ходи,  {players[lucky%2]} \nНа кону {candies_total} {choice(message)}:  '))
+            while step > max_take or step > candies_total:
+                step = int(input(f'\nЗа один ход можно взять {max_take} конфет, попробуй еще раз: '))
+        candies_total = candies_total - step
+
+    print(f'На кону осталось {candies_total} \nПобедил {players[lucky%2]}')
+
+player_vs_bot()
+'''
+
+# 3 Создайте программу для игры в ""Крестики-нолики"".
+'''
+print('*'*100)
+print('\n')
+print('А теперь давайте сыграем в крестики нолики!')
+
+board = list(range(1,10))
+
+def design_board(board):
+    print('-'*12)
+    for i in range(3):
+        print('|', board[0+i*3],'|', board[1+i*3], '|', board[2+i*3], '|')
+        print('-'*12)
+
+def choice(tic_tac):
+    valid = False
+    while not valid:
+        player_index = input('Ваш ход, выберите ячейку ' + tic_tac + ' --> ')
+        try:
+            player_index =int(player_index)
+        except:
+            print('Что то не то нажали')
+            continue
+        if player_index >= 1 and player_index <= 9:
+            if(str(board[player_index-1]) not in 'XO'):
+                board[player_index-1] = tic_tac
+                valid = True
+            else:
+                print('Занято')
+        else:
+            print('Еще раз попробуй')
+
+def victory_check(board):
+    victory = ((0,1,2),(3,4,5),(6,7,8),
+               (0,3,6),(1,4,7),(2,5,8),
+               (0,4,8),(2,4,6))
+    for i in victory:
+        if board[i[0]] == board[i[1]] == board[i[2]]:
+            return board[i[0]]
+    return False
+
+def game(board):
+    counter =0
+    vic = False
+    while not vic:
+        design_board(board)
+        if counter % 2 == 0:
+            choice('X')
+        else:
+            choice('0')
+        counter +=1
+        if counter > 4:
+            tt_win = victory_check(board)
+            if tt_win:
+                print(tt_win,'Победа')
+                vic = True
+                break
+            if counter == 9:
+                print('Победила, ДРУЖБА)')
+        design_board(board)
+game(board)
+'''
+
+# 4 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
+
+
+'''
+with open('file_encode.txt', 'w') as data:
+    data.write('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW')
+
+with open('file_encode.txt', 'r') as data:
+    string = data.readline()
+
+def rle_encode(decoded_string):
+    encoded_string = ''
+    count = 1
+    char = decoded_string[0]
+    for i in range(1, len(decoded_string)):
+        if decoded_string[i] == char:
+            count += 1
+        else:
+            encoded_string = encoded_string + str(count) + char
+            char = decoded_string[i]
+            count = 1
+            encoded_string = encoded_string + str(count) + char
+    return encoded_string
+
+
+def rle_decode(encoded_string):
+    decoded_string = ''
+    char_amount = ''
+    for i in range(len(encoded_string)):
+        if encoded_string[i].isdigit():
+            char_amount += encoded_string[i]
+        else:
+            decoded_string += encoded_string[i] * int(char_amount)
+        char_amount = ''
+    print(decoded_string)
+
+    return decoded_string
+
+
+with open('file_encode.txt', 'r') as file:
+    decoded_string = file.read()
+
+with open('file_decode.txt', 'w') as file:
+    encoded_string = rle_encode(decoded_string)
+    file.write(encoded_string)
+
+print('Decoded string: \t' + decoded_string)
+print('Encoded string: \t' + rle_encode(decoded_string))
+print(f'Compress ratio: \t{round(len(decoded_string) / len(encoded_string), 1)}')
+'''
